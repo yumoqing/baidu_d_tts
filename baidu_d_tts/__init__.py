7dd6afe784942b76845e49efa1816c8e2467011b
yumoqing <yumoqing@gmail.com>
2022-08-13 12:48:19 -0400

    bugfix

--- baidu_d_tts/__init__.py
+++ baidu_d_tts/__init__.py
@@ -2,8 +2,8 @@ import os
 import time
 from traceback import print_exc
 import tempfile
-from .baidu_ai_api import BaiduAudioApi
-from pyttsx3.voice import Voice
+from .baidu_ai_api import BaiduAudioApi, set_app_info
+from unitts.voice import Voice
 from appPublic.audioplayer import AudioPlayer
 from appPublic.background import Background
 from text2sentences import text_to_sentences
@@ -61,34 +61,33 @@ class BaiduTTSDriver:
 
 	
 	def speak_finish(self):
-		print('callback:speak_finish() called')
-		# self._proxy.notify('finished-utterance')
-		self._proxy.setBusy(False)
+		print('callback:speak_finish() called, remind sentences:', len(self.sentences))
 		try:
 			os.unlink(self.player.source)
-		except:
-			if len(self.sentences) < 1:
-				self._proxy.notify('finished-utterance')
-			pass
-		# self._pump()
+		except Exception as e:
+			print(str(e))
+
+		self._proxy.notify('finished-sentence')
 
 	def _push(self, cmd):
 		self.cmds.append(cmd)
-		# self._pump()
 
 	def _pump(self):
 		if self.player.is_busy():
+			# print('player is busy')
 			return False
 		if len(self.cmds) < 1:
 			# print('No cmd to do')
+			if self._proxy.isBusy():
+				self._proxy.setBusy(False)
+				self._proxy.notify('finished-utterence')
 			return False
 
 		pos, fn = self.cmds.pop(0)
 		self.player.set_source(fn)
 		self.player.play()
 		print('play ...', fn)
-		s = self.sentences[pos]
-		self._proxy.notify('started-sentence', s.start_pos)
+		self._proxy.notify('started-sentence', pos)
 		return True
 
 	def destroy(self):
@@ -116,40 +115,34 @@ class BaiduTTSDriver:
 			self.task.join()
 			self.task = None
 
-	def iterate(self):
-		self._proxy.setBusy(False)
-		yield
-	
 	def set_type_voice(self, attrs, sentence):
 		y = self._tts
 		y.tts_set_rate(attrs.get('rate', self.rate))
 		y.tts_set_pitch(attrs.get('pitch', self.pitch))
 		y.tts_set_voice(attrs.get('voice', self.voice))
 
-	def get_audio_file(self, pos, sentence):
+	def get_audio_file(self, sentence):
 		y = self._tts
 		if sentence.dialog:
 			self.set_type_voice(self.dialog_voice, sentence)
 		else:
 			self.set_type_voice(self.normal_voice, sentence)
 		y.tts_set_format(self.format)
-		y.tts_set_language(sentence.lang)
+		# y.tts_set_language(sentence.lang)
 		raw = y.tts(sentence.text)
 		if raw is None:
 			print('baidu api error')
 			return
 		mp3file = write_tmp_mp3file(raw)
-		self._push((pos, mp3file))
+		self._push((sentence.start_pos, mp3file))
 		return mp3file
 		
-	def say(self, text):
+	def say(self, sentence):
 		try:
+			print('baidu_d_tts driver,say() called')
 			self._proxy.setBusy(False)
 			self._completed = True
-			self.sentences = text_to_sentences(text)
-			for i, s in enumerate(self.sentences):
-				print('s=', s.text, s.lang)
-				mp3file = self.get_audio_file(i, s)
+			mp3file = self.get_audio_file(sentence)
 
 		except Exception as e:
 			print('error:', e)
